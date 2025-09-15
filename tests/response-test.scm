77d4ff231f0c9d3afe17dbd89920ea817cd177e5
Jeremy Tsang <jeremytsang1@users.noreply.github.com>
2025-09-15 05:21:45 -0700

    REFACTOR: Extract hardcoded values from existing tests

--- tests/response-test.scm
+++ tests/response-test.scm
@@ -1,16 +1,21 @@
 (use-modules (srfi srfi-64)
              (src response))
 
-(test-begin "harness-response")
+(define TEST-SUITE-NAME "harness-response")
+(define EMPTY-STRING "")
+(define EMPTY-RESPONSE "")
+(define SINGLE-WORD-RESPONSE "foo")
+
+(test-begin TEST-SUITE-NAME)
 
 (test-assert "Empty Response"
   (string=?
-   ""
-   (qnr-format-response "")))
+   EMPTY-STRING
+   (qnr-format-response EMPTY-RESPONSE)))
 
 (test-assert "Single word response"
   (string=?
-   "foo"
-   (qnr-format-response "foo")))
+   SINGLE-WORD-RESPONSE
+   (qnr-format-response SINGLE-WORD-RESPONSE)))
 
-(test-end "harness-response")
+(test-end TEST-SUITE-NAME)
