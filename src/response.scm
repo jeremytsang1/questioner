410bf206e3b246dee0c7d32a5f65a7cb0d64b1c9
Jeremy Tsang <jeremytsang1@users.noreply.github.com>
2025-09-15 19:22:51 -0700

    Green: Handle multiple spaces in between words

--- src/response.scm
+++ src/response.scm
@@ -5,10 +5,16 @@
 (define EMPTY-STRING "")
 
 (define (qnr-format-response response)
-  (if (white-space? response) EMPTY-STRING (string-trim-both response)))
+  (let* ((tokens
+          (filter (lambda (str) (not (white-space? str)))
+                  (string-split
+                   response
+                   (lambda (char)
+                     (char-set-contains? char-set:whitespace char)))))
+         (formatted (string-join tokens " ")))
+    (if (white-space? formatted) EMPTY-STRING (string-trim-both formatted))))
 
 (define (white-space? response)
   (char-set-every
    (lambda (char) (char-set-contains? char-set:whitespace char))
    (->char-set response)))
-
