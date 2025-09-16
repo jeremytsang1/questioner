2a0c03275441f4ddb6d50e6a9d446fddae7e367f
Jeremy Tsang <jeremytsang1@users.noreply.github.com>
2025-09-15 18:28:13 -0700

    GREEN: Format entirely whitespace strings down to empty string

--- src/response.scm
+++ src/response.scm
@@ -1,4 +1,10 @@
 (define-module (src response)
   #:export (qnr-format-response))
 
-(define (qnr-format-response response) response)
+(define (qnr-format-response response)
+  (if (char-set-every
+       (lambda (char) (char-set-contains? char-set:whitespace char))
+       (->char-set response))
+      ""
+      response))
+
