9e7b936e1a9e88d8c4103b82cf9bbeddd36fdc8b
Jeremy Tsang <jeremytsang1@users.noreply.github.com>
2025-09-15 19:39:41 -0700

    REFACTOR: Remove helper that is no longer necessary
    Don't actually need whitespace at the string level. This is because once
    the string is properly formatted, any string that was once composed
    entirely of whitespace has been formatted down to an empty string. That
    is `string-split` removes any whitespace, `filter` removes any empty
    strings, so if `string-join` acts on an empty list it just returns an
    empty string (which is exactly what is needed at that point).

--- src/response.scm
+++ src/response.scm
@@ -14,12 +14,7 @@
            (lambda (str) (not (string-null? str)))
            (string-split response char-whitespace?)))
          (formatted (string-join tokens SINGLE-SPACE)))
-    (if (string-whitespace? formatted) EMPTY-STRING (string-trim-both formatted))))
-
-(define (string-whitespace? response)
-  (char-set-every
-   char-whitespace?
-   (->char-set response)))
+    (string-trim-both formatted)))
 
 (define (char-whitespace? char)
   (char-set-contains? char-set:whitespace char))
