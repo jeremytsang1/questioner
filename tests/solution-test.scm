816aa643772f2ba40eebf5cabf12101855e8ad0e
Jeremy Tsang <jeremytsang1@users.noreply.github.com>
2025-09-21 00:54:46 -0700

    REFACTOR: Extract test constants

--- tests/solution-test.scm
+++ tests/solution-test.scm
@@ -8,10 +8,14 @@
 ;; Constants ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define TEST-SUITE-NAME (qnr-generate-log-file-name))
 
+(define SINGLE-CHOICE-CHOICES '("blue"))
+(define SINGLE-RESPONSE-EXPECTED-RESPONSE-COUNT)
+
 ;; Tests ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (test-begin TEST-SUITE-NAME)
 (test-equal "verify record type after construction"
   #t
-  (qnr-solution? (qnr-make-solution '("blue") 1)))
+  (qnr-solution? (qnr-make-solution SINGLE-CHOICE-CHOICES
+                                    SINGLE-RESPONSE-EXPECTED-RESPONSE-COUNT)))
 
 (test-end TEST-SUITE-NAME)
