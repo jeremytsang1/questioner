2b19578da61aa94fc50b83723ec32b9fd7511d50
Jeremy Tsang <jeremytsang1@users.noreply.github.com>
2025-09-20 22:31:20 -0700

    REFACTOR: Reword `choices` to `choice` and remove tests
    Remove tests relating to the list of choices as well as cross choice
    logic. Only deal with `choice` (singular) in these modules.

--- src/choice.scm
+++ src/choice.scm
@@ -3,84 +3,48 @@
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-9)
   #:use-module (src answer)
-  #:export (QNR-ERROR-CHOICES-WRONG-TYPE
-            QNR-ERROR-CHOICES-NO-CHOICES-FOUND
-            QNR-ERROR-SINGLE-CHOICE-WRONG-TYPE
-            QNR-ERROR-CHOICES-EMPTY
-            QNR-ERROR-CHOICES-ALTERNATIVE-WRONG-TYPE
+  #:export (QNR-VALID-CHOICE-NO-ERROR
+            QNR-ERROR-CHOICE-WRONG-TYPE
+            QNR-ERROR-CHOICE-EMPTY
+            QNR-ERROR-CHOICE-ALTERNATIVE-WRONG-TYPE
             QNR-ERROR-ALTERNATIVE-MADE-ENTIRELY-OF-WHITESPACE
             QNR-ERROR-DUPLICATE-ALTERNATIVES-ACROSS-CHOICES
-            qnr-validate-choices))
+            qnr-validate-choice))
 
 ;; Constants ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define QNR-VALID-CHOICE-NO-ERROR "") ;; TODO: Remove duplicate definition.
-(define QNR-ERROR-CHOICES-WRONG-TYPE
-  "`choices` has wrong type")
-(define QNR-ERROR-CHOICES-NO-CHOICES-FOUND
-  "`choices` empty")
-(define QNR-ERROR-SINGLE-CHOICE-WRONG-TYPE
-  "A top-level member of `choices` was given wrong type")
-(define QNR-ERROR-CHOICES-EMPTY
-  "`choices` contains an empty list choice")
-(define QNR-ERROR-CHOICES-ALTERNATIVE-WRONG-TYPE
-  "`choices` contains a choice with an alternative that has wrong type")
+(define QNR-VALID-CHOICE-NO-ERROR "")
+
+(define QNR-ERROR-CHOICE-WRONG-TYPE
+  "`choice` has wrong type")
+(define QNR-ERROR-CHOICE-EMPTY
+  "`choice` is empty")
+(define QNR-ERROR-CHOICE-ALTERNATIVE-WRONG-TYPE
+  "`choice` an alternative that has wrong type")
 (define QNR-ERROR-ALTERNATIVE-MADE-ENTIRELY-OF-WHITESPACE
-  "`choices` contains a choice with a completely whitespace alternative")
-(define QNR-ERROR-DUPLICATE-ALTERNATIVES-ACROSS-CHOICES
-  "`choices` contains duplicate alternatives across choices")
+  "`choice` contains alternative made completely of whitespace")
 
 ;; Validation ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define (qnr-validate-choices choices)
-  "Return a string containing why CHOICES has invalid choices, otherwise
-return QNR-VALID-CHOICE-NO-ERROR
+(define (qnr-validate-choice choice)
+  "Return a string containing why CHOICE is invalid, otherwise return
+QNR-VALID-CHOICE-NO-ERROR.
 
-CHOICES is valid if the following are true:
-- It is a non-empty list of non-empty lists of strings (these strings are
-  called `alternatives`). Each of the non-empty lists represents a single
-  choice.
-- None of the strings (alternatives) in the sublists are made
-  entirely of whitespacel.
-- There are no duplicate alternatives across choices
-  (one string in one sublist does not show up in another sublist)."
+CHOICE is valid if the following are true:
+
+- It is a non-empty lists of strings (these strings are called `alternatives`).
+
+- None of the strings (alternatives) are made entirely of whitespace."
   ;; Order matters in the following `cond`.
-  (cond ((not (list? choices)) QNR-ERROR-CHOICES-WRONG-TYPE)
-        ((null? choices) QNR-ERROR-CHOICES-NO-CHOICES-FOUND)
-        ((contains-non-list choices)
-         QNR-ERROR-SINGLE-CHOICE-WRONG-TYPE)
-        ((contains-empty-sublist choices)
-         QNR-ERROR-CHOICES-EMPTY)
-        ((sublist-members-contain-non-string choices)
-         QNR-ERROR-CHOICES-ALTERNATIVE-WRONG-TYPE)
-        ((sublist-members-composed-entirely-of-whitespace choices)
+  (cond ((not (list choice)) QNR-ERROR-CHOICE-WRONG-TYPE)
+        ((null? choice) QNR-ERROR-CHOICE-EMPTY)
+        ((find-non-string choice)
+         QNR-ERROR-CHOICE-ALTERNATIVE-WRONG-TYPE)
+        ((find-whitespace-string choice)
          QNR-ERROR-ALTERNATIVE-MADE-ENTIRELY-OF-WHITESPACE)
-        ((contains-duplicates-across-sublists? choices)
-         QNR-ERROR-DUPLICATE-ALTERNATIVES-ACROSS-CHOICES)
         (else QNR-VALID-CHOICE-NO-ERROR)))
 
-(define (contains-non-list lst)
-  (find (lambda (element) (not (list? element))) lst))
+(define (find-non-string choice)
+  (find (lambda (alternative) (not (string? alternative))) choice))
 
-(define (contains-empty-sublist lst)
-  (find (lambda (element) (null? element)) lst))
-
-(define (sublist-members-contain-non-string list-of-lists)
-  (find (lambda (sublist)
-          (find (lambda (alternative) (not (string? alternative)))
-                sublist))
-        list-of-lists))
-
-(define (sublist-members-composed-entirely-of-whitespace list-of-lists-of-strings)
-  (find (lambda (sublist)
-          (find (lambda (alternative)
-                  (string-null? (qnr-format-answer alternative)))
-                sublist))
-        list-of-lists-of-strings))
-
-(define (contains-duplicates-across-sublists? list-of-lists)
-  "Uses eqv? to make the comparisons."
-  (define (check-for-duplicates lst seen)
-    (cond ((null? lst) #f)
-          ((not (null? (lset-intersection eqv? (car lst) seen))) #t)
-          (else (check-for-duplicates (cdr lst)
-                                      (lset-union eqv? seen (car lst))))))
-  (check-for-duplicates list-of-lists '()))
+(define (find-whitespace-string choice)
+  (find (lambda (alternative) (string-null? (qnr-format-answer alternative)))
+        choice))
--- tests/choice-test.scm
+++ tests/choice-test.scm
@@ -3,69 +3,38 @@
 #!/usr/local/bin/guile -s
 !#
 
-(use-modules (srfi srfi-64)
-             (util test)             
-             (src choice))
+(use-modules (srfi srfi-64) (util test) (src choice))
 
 ;; Constants ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define TEST-SUITE-NAME (qnr-generate-log-file-name))
 
-(define CHOICES-INVALID-WRONG-TYPE "not a list")
-(define CHOICES-INVALID-NO-CHOICES '())
-(define CHOICES-INVALID-CHOICES-WRONG-TYPE
-  '(("foo") "bar" "bop"))
-(define CHOICES-INVALID-EMPTY-CHOICE
-  '(("foo" "bar") () '("baz" "bop")))
-(define CHOICES-INVALID-ALTERNATIVE-WRONG-TYPE
-  '((("abc")) ("1" "2" "3")))
-(define CHOICES-INVALID-EMPTY-ALTERNATIVE
-  '(("foo" "baz" "bop") ("baz") ("alpha" "" "beta")))
-(define CHOICES-INVALID-EMPTY-ALTERNATIVE-SPACES
-  '(("a" "b" " c ") ("d " "        " "e") ("f")))
-(define CHOICES-INVALID-EMPTY-ALTERNATIVE-TABS
-  '(("a" "b" " c ") ("d " "\t\t\t\t" "e") ("f")))
-(define CHOICES-INVALID-DUPLICATE-ALTERNATIVES-ACROSS-CHOICES
-  '(("alpha" "beta" "gamma")
-    ("delta")
-    ("kappa" "lambda" "alpha" "mu")))
+(define CHOICE-INVALID-EMPTY-CHOICE '())
+(define CHOICE-INVALID-ALTERNATIVE-WRONG-TYPE '(("abc")))
+(define CHOICE-INVALID-EMPTY-ALTERNATIVE '("alpha" "" "beta"))
+(define CHOICE-INVALID-EMPTY-ALTERNATIVE-SPACES '("d " "        " "e"))
+(define CHOICE-INVALID-EMPTY-ALTERNATIVE-TABS '("d " "\t\t\t\t" "e"))
 
 ;; Tests ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (test-begin TEST-SUITE-NAME)
 
-(test-equal "validate `choices` with list value"
-  QNR-ERROR-CHOICES-WRONG-TYPE
-  (qnr-validate-choices CHOICES-INVALID-WRONG-TYPE))
+(test-equal "validate `choice` cannot be empty"
+  QNR-ERROR-CHOICE-EMPTY
+  (qnr-validate-choice CHOICE-INVALID-EMPTY-CHOICE))
 
-(test-equal "validate empty list `choices`"
-  QNR-ERROR-CHOICES-NO-CHOICES-FOUND
-  (qnr-validate-choices CHOICES-INVALID-NO-CHOICES))
+(test-equal "validate `choice` containing non-string alternative"
+  QNR-ERROR-CHOICE-ALTERNATIVE-WRONG-TYPE
+  (qnr-validate-choice CHOICE-INVALID-ALTERNATIVE-WRONG-TYPE))
 
-(test-equal "validate `choices` with non-list members"
-  QNR-ERROR-SINGLE-CHOICE-WRONG-TYPE
-  (qnr-validate-choices CHOICES-INVALID-CHOICES-WRONG-TYPE))
-
-(test-equal "validate `choices` with empty sublist"
-  QNR-ERROR-CHOICES-EMPTY
-  (qnr-validate-choices CHOICES-INVALID-EMPTY-CHOICE))
-
-(test-equal "validate `choices` with sublist containing non-string"
-  QNR-ERROR-CHOICES-ALTERNATIVE-WRONG-TYPE
-  (qnr-validate-choices CHOICES-INVALID-ALTERNATIVE-WRONG-TYPE))
-
-(test-equal "validate `choices` with empty string in sublist"
+(test-equal "validate `choice` with empty string alternative"
   QNR-ERROR-ALTERNATIVE-MADE-ENTIRELY-OF-WHITESPACE
-  (qnr-validate-choices CHOICES-INVALID-EMPTY-ALTERNATIVE))
+  (qnr-validate-choice CHOICE-INVALID-EMPTY-ALTERNATIVE))
 
-(test-equal "validate `choices` with alternative made entirely of spaces"
+(test-equal "validate `choice` with alternative made entirely of spaces"
   QNR-ERROR-ALTERNATIVE-MADE-ENTIRELY-OF-WHITESPACE
-  (qnr-validate-choices CHOICES-INVALID-EMPTY-ALTERNATIVE-SPACES))
+  (qnr-validate-choice CHOICE-INVALID-EMPTY-ALTERNATIVE-SPACES))
 
-(test-equal "validate `choices` with alternative made entirely of tabs"
+(test-equal "validate `choice` with alternative made entirely of tabs"
   QNR-ERROR-ALTERNATIVE-MADE-ENTIRELY-OF-WHITESPACE
-  (qnr-validate-choices CHOICES-INVALID-EMPTY-ALTERNATIVE-TABS))
-
-(test-equal "validate `choices` with duplicate alternatives across sublists"
-  QNR-ERROR-DUPLICATE-ALTERNATIVES-ACROSS-CHOICES
-  (qnr-validate-choices CHOICES-INVALID-DUPLICATE-ALTERNATIVES-ACROSS-CHOICES))
+  (qnr-validate-choice CHOICE-INVALID-EMPTY-ALTERNATIVE-TABS))
 
 (test-end TEST-SUITE-NAME)
