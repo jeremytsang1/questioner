1eb7fafb563a4ed0b070da4f015395b7bf20acc6
Jeremy Tsang <jeremytsang1@users.noreply.github.com>
2025-09-21 01:37:43 -0700

    GREEN: Validate solution when passed more non-list types

--- tests/solution-test.scm
+++ tests/solution-test.scm
@@ -33,4 +33,17 @@
             QNR-ERROR-KEY-CHOICE-CONSTRUCTION
             (qnr-choices (qnr-make-solution 12345 1)))
 
+(test-error "fail to construct <solution> wrong type choices: string"
+            QNR-ERROR-KEY-CHOICE-CONSTRUCTION
+            (qnr-choices (qnr-make-solution "hello" 1)))
+
+(test-error "fail to construct <solution> wrong type choices: symbol"
+            QNR-ERROR-KEY-CHOICE-CONSTRUCTION
+            (qnr-choices (qnr-make-solution 'goodbye 1)))
+
+(test-error "fail to construct <solution> wrong type choices: character"
+            QNR-ERROR-KEY-CHOICE-CONSTRUCTION
+            (qnr-choices (qnr-make-solution #\a 1)))
+
+
 (test-end TEST-SUITE-NAME)
