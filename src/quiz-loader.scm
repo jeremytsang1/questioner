3dcdf934df76df80cd662c79cba7251cc3540856
Jeremy Tsang <jeremytsang1@users.noreply.github.com>
2025-09-27 01:18:14 -0700

    GREEN: Read query from a JSON quiz

--- src/quiz-loader.scm
+++ src/quiz-loader.scm
@@ -2,10 +2,14 @@
   #:use-module (json)
   #:export (QNR-ERROR-FILE-NOT-FOUND
             QNR-ERROR-INVALID-JSON
-            qnr-load-quiz))
+            qnr-load-quiz
+            qnr-question-dto-query
+            qnr-question-dto-list-questions))
 
 (define QNR-ERROR-FILE-NOT-FOUND 'qnr-error-file-not-found)
 (define QNR-ERROR-INVALID-JSON 'qnr-error-invalid-json)
+(define TOP-LEVEL-KEY-NAME "questions")
+
 
 (define (qnr-load-quiz filename)
   (catch 'json-invalid
@@ -14,7 +18,18 @@
               (catch 'system-error
                 (lambda () (open-input-file filename))
                 (lambda (key . args) (throw QNR-ERROR-FILE-NOT-FOUND))))
-             (questions-json (json->scm questions-port #:ordered #t)))
+             (parsed-json (json->scm questions-port #:ordered #t)))
         (close-port questions-port)
-        questions-json))
+        (scm->qnr-question-dto-list parsed-json)))
     (lambda (key . args) (throw 'qnr-error-invalid-json))))
+
+;; JSON Records ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define-json-type <qnr-question-dto>
+  (query)
+  (choices)
+  (expected-response-count))
+
+(define-json-type <qnr-question-dto-list>
+  (questions TOP-LEVEL-KEY-NAME #(<qnr-question-dto>)))
+
+;;  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
