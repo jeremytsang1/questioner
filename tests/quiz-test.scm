d0302e9cfdadf32592c97791aeabe54b615da5f6
Jeremy Tsang <jeremytsang1@users.noreply.github.com>
2025-09-29 13:36:11 -0700

    RED: Start test script for quiz

---
+++ tests/quiz-test.scm
@@ -0,0 +1,8 @@
+(use-modules (srfi srfi-64)
+             (util test)
+             (src quiz))
+
+(define TEST-SUITE-NAME (qnr-generate-log-file-name))
+
+(test-begin TEST-SUITE-NAME)
+(test-end TEST-SUITE-NAME)
