2ed2a54e8d3d350536e9161aa461c3102680c436
Jeremy Tsang <jeremytsang1@users.noreply.github.com>
2025-10-01 00:42:34 -0700

    GREEN: Number an empty list

--- src/quiz/quiz.scm
+++ src/quiz/quiz.scm
@@ -1,1 +1,9 @@
-(define-module (src quiz quiz))
+(define-module (src quiz quiz)
+  #:export (qnr-number-elements))
+
+(define* (qnr-number-elements lst #:optional (current-number 1))
+  "Return an alist with each key being an element of DTO-QUESTIONS and the value
+its number in the overall list with numbers starting at CURRENT-NUMBER.
+
+When CURRENT-NUMBER is ommitted, counting starts at 1."
+  '())
