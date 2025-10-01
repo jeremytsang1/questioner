4e12a2653572d01edded0da564776cee7035f92c
Jeremy Tsang <jeremytsang1@users.noreply.github.com>
2025-10-01 00:44:23 -0700

    Green: Implement numbering non-empty lists

--- src/quiz/quiz.scm
+++ src/quiz/quiz.scm
@@ -6,4 +6,6 @@
 its number in the overall list with numbers starting at CURRENT-NUMBER.
 
 When CURRENT-NUMBER is ommitted, counting starts at 1."
-  '())
+  (cond ((null? lst) '())
+        (else (cons (cons (car lst) current-number)
+                    (qnr-number-elements (cdr lst) (1+ current-number))))))
